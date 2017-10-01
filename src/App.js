460109c220598c3f7aab9c2449108a38b6a72f1e
Pete Cartwright <pete.cartwright@gmail.com>
2017-09-30 20:03:02 -0400

    Add animate.css for fancy automations

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import './animate.min.css';
 import Shortener from './Shortener';
 
 class App extends Component {
@@ -9,8 +9,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">280 is the new 140</h1>
+          <h1>280 is the new 140</h1>
         </header>
 
         <Shortener />
